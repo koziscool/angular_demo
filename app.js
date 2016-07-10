021d31d74e66cb944d975aa771ee83ba0253c958
koziscool <kozjob@yahoo.com>
2016-07-10 12:42:21 -0500

    activate controller

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 
 var firstApp = angular.module('firstApp', []);
+
+firstApp.controller('FirstCtrl', 
+    ['$scope', function($scope){
+
+      $scope.title = "hello world for realz";
+
+    }]);
+
+
